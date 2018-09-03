12aadb0120b5937cd98e03a715e917d8cfa22717
Mikhail05101990 <spech999@gmail.com>
2018-09-03 14:56:55 +0300

    data v014

--- Snippets/Csharp/Controller.js
+++ Snippets/Csharp/Controller.js
@@ -37,8 +37,75 @@ app.controller('snipcontr', function($scope)
     '\t' + 'public int UserId { get; set; }' + '\n' +
     '\t' + 'public User User { get; set; }' + '\n' +
     '}' + '\n',
-    $description: 'Users and cars'
+    $description: 'Examples of entity'
+  },
+  {
+    title: 'js functions - get, post',
+    snip: '$(document).ready(function () {' + '\n' +
+    '\n' +
+    '\t' + '$("#btn1").click(function (event) {' + '\n'  +
+    '\t' + '\t' + 'event.preventDefault();' + '\n'  + 
+    '\t' + '\t' + 'AddCar();' + '\n'  + 
+    '\t' + '});' + '\n'  +
+    '\n' +
+    '}' + '\n' +
+    '\n' +
+    'function GetAllCars() {' + '\n' +
+    '\n' +
+    '\t' + '$.ajax({' + '\n'  +
+    '\t' + '\t' + "url: '/api/Cars'," + '\n'  + 
+    '\t' + '\t' + "type: 'GET'," + '\n'  + 
+    '\t' + '\t' + "dataType: 'json'," + '\n'  + 
+    '\t' + '\t' + 'success: function (data) {' + '\n'  + 
+    '\t' + '\t' + '\t' + 'WriteResponse(data);' + '\n'  + 
+    '\t' + '\t' + '},' + '\n'  + 
+    '\t' + '\t' + "error: function (x, y, z) {" + '\n'  + 
+    '\t' + '\t' + '\t' + "alert(x + '" + '\\n' + " + y + '" + '\\n' + " + z);" + '\n'  + 
+    '\t' + '\t' + '}' + '\n'  + 
+    '\t' + '});' + '\n'  + 
+    '}' + '\n'  + 
+    '\n' +
+    'function AddCar() {' + '\n' +
+    '\n' +
+    '\t' + 'var Car = {' + '\n'  +
+    '\t' + '\t' + 'UserId: 1,' + '\n'  + 
+    '\t' + '\t' + "Model: $('#addModel').val()," + '\n'  + 
+    '\t' + '\t' + "Year: $('#addYear').val()," + '\n'  + 
+    '\t' + '\t' + "Price: $('#addPrice').val()," + '\n'  + 
+    '\t' + '\t' + "Color: $('#addColor').val()" + '\n'  + 
+    '\t' + '};' + '\n'  +
+    '\t' + '$.ajax({' + '\n'  + 
+    '\t' + '\t' + "url: '/api/Cars/'," + '\n'  +   
+    '\t' + '\t' + "type: 'POST'," + '\n'  + 
+    '\t' + '\t' + "data: JSON.stringify(Car)," + '\n'  + 
+    '\t' + '\t' + 'contentType: "application/json;charset=utf-8",' + '\n'  +
+    '\t' + '\t' + 'success: function (data) {' + '\n'  +
+    '\t' + '\t' + '\t' + 'GetAllCars();' + '\n'  + 
+    '\t' + '\t' + '},' + '\n'  + 
+    '\t' + '\t' + 'error: function (x, y, z) {' + '\n'  + 
+    '\t' + '\t' + '\t' + "alert(x + '" + '\\n' + " + y + '" + '\\n' + " + z);" + '\n'  + 
+    '\t' + '\t' + '}' + '\n'  + 
+    '\t' + '});' + '\n'  + 
+    '}' + '\n',
+    $description: 'Examples of functions'
+  },
+  {
+    title: 'Table render functions',
+    snip: 'function WriteResponse(cars) {' + '\n' +
+    '\n' +
+    '\t' + 'var strResult = "<table><th>Login</th><th>Password</th><th>Model</th><th>Year</th><th>Price</th><th>Color</th>";' + '\n'  +
+    '\t' + 'var str = "";' + '\n'  +
+    '\t' + '$.each(cars, function (index, Car) {' + '\n'  +
+    '\t' + '\t' + '$.each(Car.car, function (index, value) {' + '\n'  + 
+    '\t' + '\t' + '\t' + 'str += "<tr><td>" + Car.login + "</td><td>" + Car.password + "</td><td>" + value.model + "</td><td>" + value.year + "</td><td>" + value.price + "</td><td>" + value.color + "</td></tr>";' + '\n'  + 
+    '\t' + '\t' + '\t' + 'strResult += str;' + '\n'  + 
+    '\t' + '\t' + '\t' + 'str = "";' + '\n'  + 
+    '\t' + '\t' + '});' + '\n'  + 
+    '\t' + '});' + '\n'  +
+    '\t' + 'strResult += "</table>";' + '\n'  +
+    '\t' + '$("#tableBlock").html(strResult);' + '\n'  +
+    '}',
+    $description: 'A table is loaded by the function'
   }]
-
 
 });
