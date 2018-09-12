6519effd2ed38ffd6dfdecee0f419843cce54611
Mikhail0510 <spech999@gmail.com>
2018-09-12 22:49:12 +0300

    data 021

--- Links/Linux/Controller.js
+++ Links/Linux/Controller.js
@@ -17,14 +17,17 @@ app.controller('linker', function($scope)
     description: 'Working with devices in Ubuntu',
     ref: "http://itautsors.ru/ubuntu-montiruem-fleshkucddvdiso"
   },{
-    description: 'Writing an image of Raspberry',
+    description: 'Writting an image of Raspberry',
     ref: "http://www.armlinux.ru/создание-sd-карты-с-операционной-систем/"
   },{
-    description: 'Seting raspberry up in Windows',
+    description: 'Setting raspberry up in Windows',
     ref: "http://wiki.amperka.ru/rpi:installation"
   },{
-    description: 'Seting raspberry up in Ubuntu',
+    description: 'Setting raspberry up in Ubuntu',
     ref: "https://dgrafov.blogspot.com/2016/06/ubuntu-raspberry-pi.html"
+  },{
+    description: "Setting SSH keys on the Raspberry",
+    ref:"https://kamilslab.com/2016/12/17/how-to-set-up-ssh-keys-on-the-raspberry-pi/"
   }]
 
 this.commands = [{
@@ -36,6 +39,30 @@ this.commands = [{
   },{
     description: "Connected devices",
     text:"lsblk"
+  },{
+    description: "UUID and filesystem type of devices",
+    text:"blkid"
+  },{
+    description: "Create a catalog",
+    text:"sudo mkdir /media/RaspiFlash"
+  },{
+    description: "Writting rights",
+    text:"sudo chmod 777 /media/RaspiFlash"
+  },{
+    description: "Mount a fat partition",
+    text: "sudo mount -t vfat /dev/mmcblk0p1 /media/RaspiFlash"
+  },{
+    description: "Mount a partition with filesystem autodetecting",
+    text:"sudo mount /dev/mmcblk0p2 /media/RaspiRoot"
+  },{
+    description: "Unmount a device",
+    text:"sudo umount /media/RaspiRoot"
+  },{
+    description: "Writting in Linux",
+    text: "dd if=image.img of=/dev/sdc"
+  },{
+    description: "Deleting a file in Ubuntu",
+    text:"sudo rm -v  /media/RaspiRoot/ssh"
   }]
 
 $scope.title2 = 'Ubuntu commands'
